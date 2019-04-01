3693d30afecbb70faf7c0bde60e11055d7ab9066
Ben Bariteau <ben.bariteau@gmail.com>
2019-04-01 13:11:41 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='dict-tools',
+    version='0.1.0',
+    description='Utilities for dealing with dictionaries',
+    url='https://github.com/firba1/dict-tools',
+    author='Ben Bariteau',
+    email='ben.bariteau@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['dict_tools'],
+)
