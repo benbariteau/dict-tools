e148e32fa4ffdf79e44ac461d6d22eee55f7f164
Dull Bananas <dull.bananas0@gmail.com>
2019-07-19 17:22:36 -0700

    Add more classifiers and parameters to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    readme = f.read()
+
 setup(
     name='dict-tools',
     version='0.1.0',
     description='Utilities for dealing with dictionaries',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    keywords='dictionaries',
     url='https://github.com/firba1/dict-tools',
     author='Ben Bariteau',
     author_email='ben.bariteau@gmail.com',
@@ -13,6 +19,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=['dict_tools'],
 )
