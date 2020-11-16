0459bec7434184b99dc23a67fb39fd2917842ce5
Ben Bariteau <ben.bariteau@gmail.com>
2020-11-15 17:02:41 -0800

    move all package configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='dict-tools',
-    version='0.3.0',
-    description='Utilities for dealing with dictionaries',
-    keywords='dictionaries',
-    url='https://github.com/firba1/dict-tools',
-    author='Ben Bariteau',
-    author_email='ben.bariteau@gmail.com',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    packages=['dict_tools'],
-)
+setup()
